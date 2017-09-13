dce3a602022e7a5139fe6f887a6d3638cd2b5277
Abel Amaro Julian <jumaroz@live.com>
2017-09-12 21:55:45 -0500

    Add project files.

---
+++ DemoCommand/ViewModel/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DemoCommand.ViewModel
+{
+    internal class Demo : INotifyPropertyChanged
+    {
+        private string _id;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DemoCommand Command { get; set; } = new DemoCommand();
+
+        public string Id
+        {
+            get => _id;
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    OnPropertyChanged();
+                }
+                Command.OnCanExecuteChanged();
+            }
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DemoCommand/ViewModel/DemoCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace DemoCommand.ViewModel
+{
+    internal class DemoCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return  !string.IsNullOrWhiteSpace(parameter?.ToString());
+        }
+
+        public void Execute(object parameter)
+        {
+            Debug.WriteLine("Executing >>>>>> " + parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
